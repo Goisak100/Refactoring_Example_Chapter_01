39434675d0f29ebfe87da37af7608e5ddc4ef5df
goisak100 <goisak100@naver.com>
2023-06-29 10:39:08 +0900

    performance -> perf 로 다시 수정(문장이 너무 길어짐), amountFor() 함수를 최상위로 이동함

--- root.js
+++ root.js
@@ -34,7 +34,6 @@ export const invoices = [
     }
 ]
 
-
 export default function statement(invoice, plays) {
     const statementData = {
         customer: invoice.customer,
@@ -45,33 +44,12 @@ export default function statement(invoice, plays) {
     function enrichPerformance(performance) {
         const result = Object.assign({}, performance);
         result.play = playFor(result);
+        result.amount = amountFor(result);
         return result;
     }
 
     function playFor(performance) {
         return plays[performance.playID];
-    }
-}
-
-// plays 매개변수를 제거했다.
-// 여기에서 performance.playID를 직접 사용하지 않고, performance.play.name을 사용한다.
-// 둘 모두 결과는 동일하지만, 전자는 변수를 직접 참조하는 것이고, 후자는 함수에 의해 관리하는 것이다.
-// 즉, 어떤 변경사항이 생겼을 때(이름이 변경되는 등) 후자의 경우가 더욱 쉽게 관리할 수 있다.
-function renderPlainText(statementData) {
-    let result = `청구 내역 (고객명: ${statementData.customer})\n`;
-    for (let performance of statementData.performances) {
-        result += `${performance.play.name}: ${usd(amountFor(performance))} (${performance.audience}석)\n`;
-    }
-    result += `총액: ${usd(totalAmount())}\n`;
-    result += `적립 포인트: ${totalVolumeCredits()}점\n`;
-    return result;
-
-    function usd(number) {
-        return new Intl.NumberFormat("en-US", {
-            style: "currency",
-            currency: "USD",
-            minimumFractionDigits: 2
-        }).format(number / 100);
     }
 
     function amountFor(performance) {
@@ -97,11 +75,29 @@ function renderPlainText(statementData) {
 
         return result;
     }
+}
+
+function renderPlainText(statementData) {
+    let result = `청구 내역 (고객명: ${statementData.customer})\n`;
+    for (let perf of statementData.performances) {
+        result += `${perf.play.name}: ${usd(perf.amount)} (${perf.audience}석)\n`;
+    }
+    result += `총액: ${usd(totalAmount())}\n`;
+    result += `적립 포인트: ${totalVolumeCredits()}점\n`;
+    return result;
+
+    function usd(number) {
+        return new Intl.NumberFormat("en-US", {
+            style: "currency",
+            currency: "USD",
+            minimumFractionDigits: 2
+        }).format(number / 100);
+    }
 
     function totalAmount() {
         let result = 0;
         for (let perf of statementData.performances) {
-            result += amountFor(perf);
+            result += perf.amount;
         }
         return result;
     }
