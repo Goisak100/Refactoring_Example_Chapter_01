d88efe6ae88f8dadb086789074a27f1a8ea486d5
goisak100 <goisak100@naver.com>
2023-07-02 08:25:02 +0900

    Complete Chapter_06_05 Change Function Delcaration

--- Chapter_06/06_05/test.js
+++ Chapter_06/06_05/test.js
@@ -23,10 +23,11 @@ const customers = [
 
 // export가 붙은 함수는 함부로 선언을 수정하면 안 된다.
 export function test() {
-    const jeollabukDoPeople = customers.filter(c => isJeollabukDo(c));
+    const jeollabukDoPeople = customers.filter(c => isJeollabukDo(c.address.state));
     return jeollabukDoPeople.map(c => c.name);
 }
 
-function isJeollabukDo(customer) {
-    return ["전주", "완주"].includes(customer.address.state);
+// 함수가 훨씬 더 명확하게 변경되었다.
+function isJeollabukDo(stateCode) {
+    return ["전주", "완주"].includes(stateCode);
 }
