3eff728500f37d50b0df1b7a286c9678e3bb3844
goisak100 <goisak100@naver.com>
2023-07-07 09:09:45 +0900

    Order에서 priorityString 게터를 제거
    priorityString 게터를 제거하고, priority 게터를 생성했음. 코드
    베이스에서 priorityString을 사용하는 부분을 모두
    priority().toString()으로 변경

--- Chapter_07/07_03/test.js
+++ Chapter_07/07_03/test.js
@@ -13,7 +13,7 @@ class Order {
     }
 
     get name() { return this._name; }
-    get priorityString() { return this._priority.toString(); }
+    get priority() { return this._priority; }
 }
 
 const orders = [
@@ -24,14 +24,5 @@ const orders = [
 ]
 
 export function test() {
-    return orders.filter(o => o.priorityString === 'high' || o.priorityString === 'rush').map(o => o.priorityString);
+    return orders.filter(o => o.priority.toString() === 'high' || o.priority.toString() === 'rush').map(o => o.priority.toString());
 }
-
-
-
-// 방금과 같은 상황에서 내가 뭘 해야 하는지 몰랐어.
-// 그렇지만, 답을 알려달라고 한 게 아니라
-// 내가 분명 filter를 잘못 사용하고 있는 거라 생각하고, filter 사용법을 찾아봤어.
-// 이거는 스스로가 문제를 해결한 것인가? 아니면, 답을 보고 판단한 것인가?
-
-// 컴퓨터는 거짓말을 하지 않는다. 단지, 내가 그 방법을 정확하게 인지하지 못하고 있기 때문에 문제가 발생한다.
